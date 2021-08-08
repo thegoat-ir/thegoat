d4a8afbc1538e5f7fffccf7202a1d0cf8d27a1aa
Mahdyar Hasanpour <hi@mahdyar.me>
2021-08-08 21:57:13 +0430

    chore: update license in comments

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 The Goat <we@thegoat.ir>
+Copyright © 2021 Mahdyar Hasanpour <hi@mahdyar.me>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
